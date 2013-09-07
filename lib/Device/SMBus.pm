8c79913b7e01e5b8823d7e2db0a09b68ac78912c
Shantanu Bhadoria <shantanu@cpan.org>
2013-09-08 00:58:33 +0800

    First Release with bare minumum function

---
+++ lib/Device/SMBus.pm
@@ -0,0 +1,129 @@
+use strict;
+use warnings;
+package Device::SMBus;
+
+# PODNAME: Device::SMBus 
+# ABSTRACT: Perl interface for smbus using libi2c-dev library.  
+# COPYRIGHT
+# VERSION
+
+use 5.010000;
+
+# Dependencies
+use Mo;
+use Carp;
+
+use IO::File;
+use Fcntl;
+
+require XSLoader;
+XSLoader::load('Device::SMBus', $VERSION);
+
+use constant I2C_SLAVE => 0x0703;
+
+=attr I2CBusDevicePath
+
+Device path of the I2C Device. 
+
+ * On Raspberry Pi Model A this would usually be /dev/i2c-0 if you are using the default pins.
+ * On Raspberry Pi Model B this would usually be /dev/i2c-1 if you are using the default pins.
+
+=cut
+
+has I2CBusDevicePath   => (
+    is => 'ro',
+);
+
+has I2CBusFileHandle => (
+    is         => 'ro',
+    lazy_build => 1,
+);
+
+=attr I2CDeviceAddress
+
+This is the Address of the device on the I2C bus, this is usually available in the device Datasheet.
+
+ * for /dev/i2c-0 look at output of `sudo i2cdetect -y 0' 
+ * for /dev/i2c-1 look at output of `sudo i2cdetect -y 1' 
+
+=cut
+
+has I2CDeviceAddress => (
+    is => 'ro',
+);
+
+has I2CBusFilenumber => (
+    is => 'ro',
+);
+
+sub _build_I2CBusFileHandle {
+    my ($self) = @_;
+    my $fh = IO::File->new( $self->I2CBusDevicePath, O_RDWR );
+    if( !$fh ){
+        croak "Unable to open I2C Device File at $self->I2CBusDevicePath";
+        return -1;
+    }
+    $self->I2CBusFilenumber($fh->fileno());
+    $fh->ioctl(I2C_SLAVE,$self->I2CDeviceAddress);
+    return $fh;
+}
+
+=method readByteData
+
+$self->readByteData($register_address)
+
+=cut
+
+sub readByteData {
+    my ($self,$register_address) = @_;
+    my $retval = Device::SMBus::_readByteData($self->I2CBusFilenumber,$register_address);
+}
+
+=method writeByteData
+
+$self->writeByteData($register_address,$value)
+
+=cut
+
+sub writeByteData {
+    my ($self,$register_address,$value) = @_;
+    my $retval = Device::SMBus::_readByteData($self->I2CBusFilenumber,$register_address,$value);
+}
+
+sub DEMOLISH {
+    my ($self) = @_;
+    $self->I2CBusFileHandle->close();
+}
+
+1;
+
+__END__
+
+=begin wikidoc
+
+= SYNOPSIS
+
+  use Device::SMBus;
+  $dev = Device::SMBus->new(
+    I2CBusDevicePath => '/dev/i2c-1',
+    I2CDeviceAddress => 0x1e,
+  );
+  print $dev->readByteData(0x20);
+
+= DESCRIPTION
+
+This is a perl interface to smbus interface using libi2c-dev library. 
+
+= USAGE
+
+* This module provides a simplified object oriented interface to the libi2c-dev library for accessing electronic peripherals connected on the I2C bus. It uses Mo, a scaled down version of Moose without any data checks to improve speed.
+
+= see ALSO
+
+* [Mo]
+* [IO::File]
+* [Fcntl]
+
+=end wikidoc
+
+=cut
